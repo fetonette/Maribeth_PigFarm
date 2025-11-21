c2f9c39eb7c863cae0967effd98e8a77d603c4d3
Fetonnette <fetonettet@gmail.com>
2025-11-21 14:10:54 +0800

    pls

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+# Use DATABASE_URL provided by Render
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
